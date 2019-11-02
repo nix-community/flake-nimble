e2051fe10bb80578024731331d20c80c0d6f0252
Emery Hemingway <ehmry@posteo.net>
2019-11-02 19:26:32 +0100

    update flake to apply-packages

--- flake.nix
+++ flake.nix
@@ -3,32 +3,15 @@
 
   description = "Nimble packages";
 
-  inputs.nixpkgs = {
+  inputs = {
+  nixpkgs = {
     uri = "git+https://github.com/nixos/nixpkgs.git";
     flake = false;
-  };
+  }; };
 
   outputs = { self, nixpkgs }:
-    with (import nixpkgs { system = "x86_64-linux"; });
-    let
-      nimbleDerivation = args:
-      stdenv.mkDerivation {
-        inherit (args) name meta;
-        src = fetchgit {
-          inherit (args.src) url;
-          rev = "master";
-          sha256 = lib.fakeSha256;
-        };
-      };
-
-      importPackage = path:
-        let args = import path;
-        in {
-          inherit (args) name;
-          value = nimbleDerivation args;
-        };
-
-      pkgsPaths = import ./packages;
-      pkgsList = map importPackage pkgsPaths;
-    in { packages = builtins.listToAttrs pkgsList; };
+  {
+    packages = import ./packages.nix { inherit nixpkgs; };
+    defaultPackage = self.packages.nim;
+  };
 }
