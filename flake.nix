e3046dc17d6922a9f6d83ec1fbc77fed3574aa1c
Emery Hemingway <ehmry@posteo.net>
2019-11-07 21:53:26 +0100

    Use multiplatform flake structure

--- flake.nix
+++ flake.nix
@@ -3,17 +3,18 @@
 
   description = "Nimble packages";
 
-  inputs = {
-  nixpkgs = {
-    uri = "git+https://github.com/nixos/nixpkgs.git";
-    flake = false;
-  }; };
+  inputs = { nixpkgs.uri = "git+https://github.com/edolstra/nixpkgs.git"; };
 
   outputs = { self, nixpkgs }:
-  {
-    packages = import ./default.nix {
-      nixpkgs = import nixpkgs { system = "x86_64-linux"; };
+    let
+      systems = [ "x86_64-linux" ];
+
+      forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system);
+
+    in {
+      packages = forAllSystems (system:
+        import ./default.nix { nixpkgs = import nixpkgs { inherit system; }; });
+
+      defaultPackage = forAllSystems (system: self.packages."${system}".nim);
     };
-    defaultPackage = self.packages.nim;
-  };
 }
