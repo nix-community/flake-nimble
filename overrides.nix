f4f687b41420daad4a5501a9cfa3454d7605c26e
Emery Hemingway <ehmry@posteo.net>
2022-01-13 16:59:06 +0100

    Add tkrzw override for buildInputs

--- overrides.nix
+++ overrides.nix
@@ -1,8 +1,10 @@
 pkgs: {
-  hottext.HOTTEXT_FONT_PATH =
-    "${pkgs.gentium}/share/fonts/truetype/GentiumPlus-R.ttf";
   opusenc.propagatedBuildInputs = with pkgs; [ libopus libopusenc pkgconfig ];
   sdl2.propagatedBuildInputs = with pkgs; [ SDL2 ];
+  tkrzw = with pkgs; {
+    propagatedBuildInputs = [ tkrzw ];
+    nativeBuildInputs = [ pkg-config ];
+  };
   toxcore.propagatedBuildInputs = with pkgs; [ libtoxcore ];
   inim.patches = [ ./patches/inim.patch ];
 }
