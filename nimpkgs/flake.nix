d6e9cdd7268384d2dc8e36f4d25a9329e56ed7db
hugosenari <hugosenari@gmail.com>
2022-03-02 04:03:01 -0300

    feat(nimpkgs): lock

--- nimpkgs/flake.nix
+++ nimpkgs/flake.nix
@@ -1,5 +1,4 @@
 {
-  inputs.dsf.url = "github:cruel-intentions/devshell-files";
   outputs = { self, nixpkgs, ...}@inputs:
     let lib = import ./lib.nix;
     in lib.mkMainOutput { inherit self nixpkgs inputs; };
